02f812d131389686edceebd3cd1eb34afe407ca5
Daniel <danlikestech@gmail.com>
2020-01-20 18:36:45 -0500

    started a guessing game using javascript

---
+++ Lessons/javascript/guessingGame/game.js
@@ -0,0 +1,17 @@
+// Create secret number
+var secretNumber = 4;
+
+// Ask user to guess
+var guess = prompt ("Guess a number");
+alert(guess);
+
+// Check if guess is right
+
+if (guess === secretGuess) {
+    alert("YOU GOT IT RIGHT!");
+}
+
+
+
+
+
