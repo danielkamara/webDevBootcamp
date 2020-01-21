b7b84386bfc14ba5a2728a7deb57863c9b087f12
Daniel <danlikestech@gmail.com>
2020-01-20 21:51:05 -0500

    completed guessing game

--- Lessons/javascript/guessingGame/game.js
+++ Lessons/javascript/guessingGame/game.js
@@ -2,16 +2,25 @@
 var secretNumber = 4;
 
 // Ask user to guess
-var guess = prompt ("Guess a number");
+var stringGuess = (prompt ("Guess a number"));
+var guess = Number(stringGuess);
 
 
 // Check if guess is right
 
-if (Number(guess) === secretNumber) {
+if (guess === secretNumber) {
     alert("YOU GOT IT RIGHT!");
 }
 
+// Otherwise Check if guess is higher
 
+else if(guess > secretNumber) {
+alert("Too high. Guess again!")
+}
 
+// Otherwise check if lower
+else {
+    alert("Too Low. Guess Again!")
+}
 
 
