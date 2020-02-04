a33a2775303bcda276c81bc21a2b986872e0cfdc
Daniel <danlikestech@gmail.com>
2020-02-03 23:09:51 -0600

    saving while loops section

---
+++ Lessons/javascript/whileLoops/loops.js
@@ -0,0 +1,29 @@
+console.log("Printing all numbers between -10 & 19");
+var num = -10;
+
+while(num <= 19) {
+    console.log(num)
+    num++;
+
+}
+
+console.log("Printing all even numbers between 10 & 40");
+var num = 10;
+
+while (num <= 40) {
+    console.log(num);
+    num+=2;
+
+}
+
+
+console.log("Printing all odd numbers between 300 & 333");
+var num = 300;
+
+while (num <= 333) {
+    if(num % 2 !== 0){
+    console.log(num);
+    }
+    num += 1;
+
+}
