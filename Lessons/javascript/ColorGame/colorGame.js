8675f7c3eb09ed52ad96860b4182e3d611f1e066
Daniel <danlikestech@gmail.com>
2020-03-11 22:53:04 -0500

    added more logic to game

--- Lessons/javascript/ColorGame/colorGame.js
+++ Lessons/javascript/ColorGame/colorGame.js
@@ -1,16 +1,16 @@
 var colors = [
-    "rgb(255,0,0)",
-    "rgb(255,255,0)",
-    "rgb(0,255,0)",
-    "rgb(0,255,255)",
-    "rgb(0,0,255)",
-    "rgb(255,0,255)"
+    "rgb(255, 0, 0)",
+    "rgb(255 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
-
+var messageDisplay = document.getElementById("message");
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
@@ -20,13 +20,27 @@ for (var i = 0; i < squares.length; i++) {
     // add click listeners to squares 
     squares[i].addEventListener("click", function() {
         // grab color of clicked square 
-       var clickedColor = this.style.background;
-        // compare color to picked color `
-        if(clickedColor === pickedColor) {
-            alert("CORRECT!!!");
+        var clickedColor = this.style.background;
+        // compare color to picked color 
+        if (clickedColor === pickedColor) {
+            messageDisplay.textContent = "Correct!!!";
+            changeColors(clickedColor);
         } else {
-            alert("WRONG!!!");
+            this.style.background = "#232323";
+            messageDisplay.textContent = "Try Again";
         }
-        
     });
 }
+
+function changeColors(color) {
+    // loop through all squares
+    for (var i = 0; i < squares.length; i++) {
+        // change each color to match given color
+        squares[i].style.background = color;
+    }
+}
+
+function pickColor(){
+var random = Math.floor(Math.random() * colors.length);
+return colors[random];
+} 
