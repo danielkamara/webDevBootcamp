86c4874bf21c7847681d63f17cf6cd3753f554d2
Daniel <danlikestech@gmail.com>
2020-03-17 06:31:56 -0500

    add button for user to start new game

--- Lessons/javascript/ColorGame/colorGame.js
+++ Lessons/javascript/ColorGame/colorGame.js
@@ -1,16 +1,26 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-];
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");
+var resetButton = document.getElementById("reset");
+
+resetButton.addEventListener("click", function () {
+    // generate all new colors
+    colors = generateRandomColors(6);
+    // pick a new random color from array
+    pickedColor = pickColor();
+    // changed color display to match picked color
+    colorDisplay.textContent = pickedColor;
+    // change color of squares 
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.background = colors[i];
+    }
+h1.style.background = "#232323";
+
+});
+
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
@@ -18,13 +28,16 @@ for (var i = 0; i < squares.length; i++) {
     squares[i].style.background = colors[i];
 
     // add click listeners to squares 
-    squares[i].addEventListener("click", function() {
+    squares[i].addEventListener("click", function () {
         // grab color of clicked square 
         var clickedColor = this.style.background;
         // compare color to picked color 
         if (clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct!!!";
+            resetButton.textContent = "Play Again ?"
             changeColors(clickedColor);
+            h1.style.background = clickedColor;
+
         } else {
             this.style.background = "#232323";
             messageDisplay.textContent = "Try Again";
@@ -40,7 +53,31 @@ function changeColors(color) {
     }
 }
 
-function pickColor(){
-var random = Math.floor(Math.random() * colors.length);
-return colors[random];
-} 
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function generateRandomColors(num) {
+    // make array 
+    var arr = [];
+    // repeat num times
+    for (var i = 0; i < num; i++) {
+        // get random color and push into arr 
+        arr.push(randomColor());
+    }
+    // return that array 
+    return arr;
+}
+
+function randomColor() {
+    // pick a "red" from 0 - 255
+    var r = Math.floor(Math.random() * 256);
+    // pick a "green" from 0 - 255
+    var g = Math.floor(Math.random() * 256);
+    // pick a "blue" from 0 - 255
+    var b = Math.floor(Math.random() * 256);
+
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+
+}
