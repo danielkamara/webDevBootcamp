74d7c1933752caff17b6fa0e6a50304ea61c94f8
Daniel <danlikestech@gmail.com>
2020-03-25 22:25:03 -0500

    saving lesson

--- Lessons/MoreExpress/EJSDemo/app.js
+++ Lessons/MoreExpress/EJSDemo/app.js
@@ -2,12 +2,30 @@ let express = require("express");
 let app = express();
 
 app.get("/", (req, res) => {
-res.render("home.ejs");
+    res.render("home.ejs");
 });
 
 app.get("/fallinlovewith/:thing", (req, res) => {
     let thing = req.params.thing;
-    res.render("love.ejs", {thingVar: thing});
+    res.render("love.ejs", {
+        thingVar: thing
+    });
+    let post = [{
+            title: "I don't know what i'm doing!!!!",
+            author: "Daniel Kamara"
+        },
+        {
+            title: "Reasonable Doubt",
+            author: "Jay-Z"
+        },
+        {
+            title: "Crazy In Love",
+            author: "Beyonce"
+        },
+    ]
+});
+
+app.get("/post", (req, res) => {
 
 })
 
--- Lessons/MoreExpress/EJSDemo/readme.md
+++ Lessons/MoreExpress/EJSDemo/readme.md
@@ -1,4 +1,11 @@
-#rendering HTML and Templates 
+# Rendering HTML and Templates 
+
 * Use res.render() to render HTML (from an EJS file)
 * Explain what EJS is and why we use it.
 * Pass variables to EJS templates
+
+# EJS Controll flow 
+
+* Show examples of control flow in EJS templates
+* Write if statements in an EJS file
+* Write loops in a EJS file 
