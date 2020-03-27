a1da816ea0726f8d7d242eb36783c6f4f65b62e8
Daniel <danlikestech@gmail.com>
2020-03-27 18:32:19 -0500

    sent a POST request using a HTML form

--- Lessons/MoreExpress/PostRequestDemo/app.js
+++ Lessons/MoreExpress/PostRequestDemo/app.js
@@ -1,7 +1,13 @@
 let express = require("express");
 let app = express();
+let bodyParser = require("body-parser"); 
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+let friends = ["Ryan", "Fari", "Charles", "Kevin", "Kwame"];
 
 app.get("/", (req, res) => {
 res.render("home");
@@ -9,11 +15,12 @@ res.render("home");
 
 
 app.post("/addfriend",(req, res) => {
-res.send("YOU HAVE REACHED THE POST ROUTE!!!");
+    let newFriend = req.body.newfriend;
+    friends.push(newFriend);
+    res.redirect("/friends");
 });
 
 app.get("/friends", (req, res) => {
-let friends = ["Ryan", "Fari", "Charles", "Kevin", "Kwame"];
 res.render("friends", {friends: friends});
 });
 
