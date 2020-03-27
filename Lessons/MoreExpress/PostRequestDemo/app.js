c092f7111afc84ddac329b1d3700c54e4db06007
Daniel <danlikestech@gmail.com>
2020-03-27 18:02:10 -0500

    saving lesson

--- Lessons/MoreExpress/PostRequestDemo/app.js
+++ Lessons/MoreExpress/PostRequestDemo/app.js
@@ -7,15 +7,16 @@ app.get("/", (req, res) => {
 res.render("home");
 })
 
+
+app.post("/addfriend",(req, res) => {
+res.send("YOU HAVE REACHED THE POST ROUTE!!!");
+});
+
 app.get("/friends", (req, res) => {
 let friends = ["Ryan", "Fari", "Charles", "Kevin", "Kwame"];
 res.render("friends", {friends: friends});
 });
 
-
-app.post("/friends", (req, res) => {
-    
-})
 
 app.listen(3000, () => {
     console.log("Server is running!!!!");
