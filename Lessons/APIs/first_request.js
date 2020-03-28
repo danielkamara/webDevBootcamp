3fefecb8ab5f452e4cda7c1e975b4feceecc96e1
Daniel <danlikestech@gmail.com>
2020-03-28 14:24:18 -0500

    learned how to grab data from an API, (used FreeCodeCamp's Weather API)

--- Lessons/APIs/first_request.js
+++ Lessons/APIs/first_request.js
@@ -1,8 +1,9 @@
 const request = require('request');
-request('https://www.google.com', (error, response, body) => {
+request('https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139', (error, response, body) => {
     console.error('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
+    var parsedData = JSON.parse(body);
+    console.log(parsedData.weather); // Print the HTML for the homepage.
 });
 
 
