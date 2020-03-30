e6778ca09aab94a949abaf5cd7fb59d397105e75
Daniel <danlikestech@gmail.com>
2020-03-29 23:01:28 -0500

    completed lesson, noticed if user adds invalid searches app breaks. will fix in future

--- Lessons/APIs/MovieSearchApp/app.js
+++ Lessons/APIs/MovieSearchApp/app.js
@@ -3,14 +3,21 @@ let app = express();
 let request = require("request");
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+    res.render("search");
+});
 
 app.get("/results", (req, res) => {
-request("http://www.omdbapi.com/?s=california&apikey=2b201c09", (error, response, body) => {
-console.error('error:', error); // Print the error if one occurred
-console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-let results = JSON.parse(body);
-res.send(results["Search"][0]["Title"]);
-});
+    let query = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=2b201c09";
+    request(url, (error, response, body) => {
+        console.error('error:', error);
+        console.log('statusCode:', response && response.statusCode);
+        let data = JSON.parse(body);
+        res.render("results", {
+            data: data
+        });
+    });
 });
 
 
