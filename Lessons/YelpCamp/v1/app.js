c0d24436cfb909502b8c12917e0ebbaa5c981e16
Daniel <danlikestech@gmail.com>
2020-03-30 16:23:20 -0500

    added more features (notes in readme.md)

--- Lessons/YelpCamp/v1/app.js
+++ Lessons/YelpCamp/v1/app.js
@@ -21,7 +21,28 @@ let campgrounds = [{
     {
         name: "Mountain Goat's Rest",
         image: "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201__340.jpg"
+    },
+    {
+        name: "Salmon Creek",
+        image: "https://cdn.pixabay.com/photo/2016/11/08/05/03/adventure-1807495__340.jpg"
+    },
+    {
+        name: "Granite Hill",
+        image: "https://cdn.pixabay.com/photo/2017/08/07/02/34/people-2598902__340.jpg"
+    },
+    {
+        name: "Mountain Goat's Rest",
+        image: "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201__340.jpg"
+    },
+    {
+        name: "Granite Hill",
+        image: "https://cdn.pixabay.com/photo/2017/08/07/02/34/people-2598902__340.jpg"
+    }, {
+        name: "Mountain Goat's Rest",
+        image: "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201__340.jpg"
     }
+
+
 ]
 
 
