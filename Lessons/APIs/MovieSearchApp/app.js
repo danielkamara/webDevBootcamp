feb33e9311d96e40d84b38150582944e18d85d99
Daniel <danlikestech@gmail.com>
2020-03-29 21:14:45 -0500

    saving changes on movie app/ fixed routing issue

--- Lessons/APIs/MovieSearchApp/app.js
+++ Lessons/APIs/MovieSearchApp/app.js
@@ -1,14 +1,15 @@
 let express = require("express");
 let app = express();
 let request = require("request");
+app.set("view engine", "ejs");
 
 
 app.get("/results", (req, res) => {
-request("http://www.omdbapi.com/?i=tt3896198&apikey=2b201c09", (error, response, body) => {
+request("http://www.omdbapi.com/?s=california&apikey=2b201c09", (error, response, body) => {
 console.error('error:', error); // Print the error if one occurred
 console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 let results = JSON.parse(body);
-res.send(results);
+res.send(results["Search"][0]["Title"]);
 });
 });
 
